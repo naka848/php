80be28ec11f38e476d979fc6f00130c5000e5a2b
nigaya <n.moeko1966@gmail.com>
2021-10-07 16:04:35 +0900

    10/7 変更

--- laravelapp/routes/web.php
+++ laravelapp/routes/web.php
@@ -13,3 +13,11 @@ Route::get('hello','HelloController@index');
     // ->middleware(HelloMiddleware::class);
 
 Route::post('hello','HelloController@post');
+
+Route::get('hello/add','HelloController@add');
+
+Route::post('hello/add','HelloController@create');
+
+Route::get('hello/edit','HelloController@edit');
+
+Route::post('hello/edit','HelloController@update');
